3078f452e60525f7978f6231937fbd7426905b95
Revan-000 <61224041+Revan-000@users.noreply.github.com>
2021-01-10 18:10:21 +0400

    hhhhh

--- public_html/resource/js/bs4/general_1.js
+++ public_html/resource/js/bs4/general_1.js
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var urlGl = "https://app.sourcedagile.com/";
-//var urlGl = "http://localhost:8080/tsn1/";
+//var urlGl = "https://app.sourcedagile.com/";
+ var urlGl = "http://localhost:8080/tsn1/";
 //var urlGl = ""
 
  
