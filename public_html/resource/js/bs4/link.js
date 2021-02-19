892db9207846a3f44018eca2b02c594e27660a8d
Revan-000 <61224041+Revan-000@users.noreply.github.com>
2021-02-19 05:41:06 +0400

    saas

--- public_html/resource/js/bs4/link.js
+++ public_html/resource/js/bs4/link.js
@@ -5,11 +5,11 @@
  */
 
 
-//var urlGl = "https://app.sourcedagile.com/";
-var urlGl = "http://localhost:8080/tsn1/";
+var urlGl = "https://app.sourcedagile.com/";
+//var urlGl = "http://localhost:8080/tsn1/";
 //var urlGl = ""
 
 function getToken() {
-    return 'apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.05eOJjllxbQlge6v2MCwIiSlu6joxdvw5aK9m2bjlCx4Y_GH6vMhyA.pdYzkcQZkZzt0bxUZ3smWw.-gbNwXEhor-EBetCcH6RKnRSlN54gZmmZeHrdK_BOLso5ZgPXPFziiDpM-bxFvsqJbJ7eyrz4ly92j8o_H7iGc5Zu4XwQ6sEYMSRZ5tlsZecDRkHaSJrhLSCCHvZkJydze9GCbLS94dk8rnktH9BlZKag3l1h4A7M0DsXRblYxA.7mr0qZrNeIHj7cDRf6dztw';
+    return 'apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.hNM4AkCt-mHeqYNroiFA7r-G0WadtM2hHYnu8vls_BT0A4PYnI6qrw.T2fyQSwJrEmPPOy5uAMrzw.a_ZVNPkQ6BSW4sc9sIVcu7bCNFoOeOntaPDpaIYYUDECXID8R8uydCGyRHSRKJaYfjOlPKyuOCDIucPAf8qF2pEV3fXxdtFrioE9Y4rJQW5dw7to2UtLz9T-zj7jrhg2ksqoroOkOHK8vc8rSShr6argixN-6ssi1kOsGwJagR0.ClcBckEpt1i6BM_3JPHZeQ';
     //    return '';
 }
