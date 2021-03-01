a71a0fa506b64c28ce636857d2929dbb7805fdef
Revan-000 <61224041+Revan-000@users.noreply.github.com>
2021-03-01 17:01:27 +0400

    editor

--- public_html/resource/js/bs4/link.js
+++ public_html/resource/js/bs4/link.js
@@ -5,11 +5,11 @@
  */
 
 
-//var urlGl = "https://app.sourcedagile.com/";
-var urlGl = "http://localhost:8080/tsn1/";
+var urlGl = "https://app.sourcedagile.com/";
+//var urlGl = "http://localhost:8080/tsn1/";
 //var urlGl = ""
 
 function getToken() {
-    return 'apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.FiiAxw6JJjlFjcVC7P7Y6eNKQfcDAuBu4TQiN58GGPAEL1vLXDxUXQ.1g-rIhZ9vfkofn6BkzuxjQ.FQ1XLXZ_o2inQYU8Knk2DVyh7DqM4KJfyFt__Tl5XPN0orbluqib20247IpN1HjDWHlepJ10sQokk3NeER1hrpqRs_qeg2BUlU_oYO3HD78GxpwoU-iKKkT2sxbLNeIBEj4QRQgAOJ4cjSrtPi4BweMJFLt2CGvJ4MJu3WCZPQ4.4_IbzeZCMBLfDYUAOLyurw';
+    return 'apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.qXGUFfRNAxtINRZhc0WdzB28af_wITrcwbNAfQdokfT6g7QSgdEkwA.7p4Ptf7Ji56McqZn30AtwA.yll8rczmh2kZ6mMtv_WJnwZ6i4vGD8iS3jllS1IERiVzZa-H6KQ5Bxtvy63-lR5WeNmrkztREazgceul62SLeYAoBfs6qSohQr7XdbA-Wtsdty30yPyUyOfoFS7eu85ylDuqMgzngtNldHY2pkrRO5tYh_ajzm0_3IaRx2Z6HsQ.Zkgo4t3x4rCoRPOoIlkPPQ';
     //    return '';
 }
