a6a844dc813da75587d28fa91803eaa10d79b63e
Anar <Anar@DESKTOP-1R0ML18>
2021-05-28 10:51:21 +0400

    Merge origin/master
    Conflicts:
    	public_html/resource/js/bs4/a_1.js

--- public_html/resource/js/bs4/link.js
+++ public_html/resource/js/bs4/link.js
@@ -10,7 +10,9 @@ var urlGl = "https://app.sourcedagile.com/";
 //var urlGl = ""
 
 function getToken() {
+ 
 //     return 'apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.IdaB_oQQCO-FE5ec-YA-0nbRKlt8P7Co6-T4ZAlIjwpQH3R6PA1BaA.-fVcbO5JMnfqGBpTREi6Ag.dL5pB85ykKzmBiZkym_R9PRvgxxB5q7iXn84plJ1ZbzNJM6E4JXbqe7epYcFBSSDIXMksvP-MU9j9XygBCVGWDk_0Jur7df547RODcUZ10wcvz04XlYvXPJFJQA1sQ6tjb7W6qJvEeakrqZ5HljaL5zOgaHmpKJ3HsYClrN4ESc.MyH0Gsj-hmarQyZYsHV18Q';
+ 
 //        return '';
     //return document.cookie;
     
