cd7abe455b580d007b10dd4b8e5988bb35b3851d
Anar <Anar@DESKTOP-1R0ML18>
2021-07-31 09:46:35 +0400

    Update index.html

--- public_html/resource/js/bs4/sourced-common_1.js
+++ public_html/resource/js/bs4/sourced-common_1.js
@@ -19129,7 +19129,7 @@ onclick="new UserStory().getStoryInfo(\'' + o.id + '\',this)">';
             async: true,
             success: function (res) {
                 that.getBacklogDetailedInputInfoById_core(res);
- 
+
             }
         });
     },
