21f5054507de922110f7a2d8d06928e30d43065c
revangozelov <61224041+revangozelov@users.noreply.github.com>
2021-08-17 08:19:04 +0400

    sdgsdgsdgsdgsg

--- public_html/resource/js/bs4/stats.js
+++ public_html/resource/js/bs4/stats.js
@@ -228,6 +228,14 @@ var Statistics = {
                 this.GetProjectSummaryByAssignee();
                 Statistics.LoadAssigneesByProject();
             }
+            var  Lst = $(el).val();
+          
+            $('#statistics-BacklogList').html("")
+            for (let index = 0; index < Lst.length; index++) {
+                loadDetailsOnProjectSelect4Dashboard(Lst[index]);
+                
+            }
+       
         },
         GetProjectList: function () {
             var list = $('#statistics-projectlist').val();
