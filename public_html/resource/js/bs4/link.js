a0faed658008ac42863e74103bc17a8d12004abb
talehhasan <47014159+talehhasan@users.noreply.github.com>
2021-10-04 00:00:57 +0400

    export, import and history btn

--- public_html/resource/js/bs4/link.js
+++ public_html/resource/js/bs4/link.js
@@ -8,12 +8,10 @@
 
 var urlGl = "https://test.sourcedagile.com/";     
 
-//var urlGl = "http://localhost:8079/tsn3/";
+//var urlGl = "http://192.168.1.3:8080/tsn3/";
 //var urlGl = ""
 
-function getToken() {
-//return 'eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.lrqt7yBlMgvPtZaSaPfClLjANpB0bVS_T61w-3Jchm7beM7oHLhpBw.TNvcz0HJ8sLnezGnJBzxcw.3ifV-KF6wmpPLlbC2XGnA2hRR5meEHajIa_LeEXxnQi5PcFzYYazfW4VHzOKuDg0M8zzWjTeOEdgINXfuufZcrKAVJ6d9tZfXpJYDS3YnEwOznU9ABTgGOslwsXmu8B5ZIEGTAjI1vxBwttEVg_aJ_04vFGEjhHUBfoyiKfYVH8.hCduPtCbiKgpqGd1Ydn1YQ';
-//        return '';
+function getToken() { 
     //return document.cookie;    
     return localStorage.getItem('tk');
 }
