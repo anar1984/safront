68a3b48a8d7d78783033f74e63c15df72d669d94
talehhasan <47014159+talehhasan@users.noreply.github.com>
2021-10-18 18:19:21 +0400

    editor and pagenation

--- public_html/resource/js/bs4/livesql.js
+++ public_html/resource/js/bs4/livesql.js
@@ -318,13 +318,104 @@ function getDataTableRowList(dbname, tablename, selectedField, dataCore) {
 
 $(document).on("click", ".cs-sql-editor-run-btn", function (e) {
      $('.cs-sql-editor-run-btn').toggleClass('active');
-      $(".live-sql-ditor").show('slide', {
+      $(".app_editor_wrapper").show('slide', {
         direction: 'up'
       }, 200);
     });
         
 $(document).on("click", ".cs-sql-editor-run-btn.active", function (e) {
-      $('.live-sql-ditor').hide('slide', {
+      $('.app_editor_wrapper').hide('slide', {
         direction: 'up'
       }, 200);
+    });
+
+
+    $( document ).ready(function() {
+
+        var aceEditorInstance = ace.edit("livesqlditor");
+        aceEditorInstance.setTheme("ace/theme/sqlserver");
+    
+        aceEditorInstance.getSession().setMode( "ace/mode/sql");
+    
+        window.draggingAceEditor = {};
+    
+    function makeAceEditorResizable(editor){
+        var id_editor = editor.container.id;
+        var id_dragbar = id_editor + '_dragbar';
+        var id_wrapper =  id_editor + '_wrapper';
+        var wpoffset = 0;
+        window.draggingAceEditor[id_editor] = false;
+    
+        var action_mousedown = function(e) {
+            e.preventDefault();
+    
+            window.draggingAceEditor[id_editor] = true;
+    
+            // Set editor opacity to 0 to make transparent so our wrapper div shows
+            document.getElementById(id_editor).style.opacity = 0;
+        
+            document.addEventListener("mousemove", action_document_mousemove);
+        };
+    
+        var action_document_mousemove = function(e){
+            var _editor = document.getElementById(id_editor);
+            var rect = _editor.getBoundingClientRect();
+    
+            var rsl = {
+                top: rect.top + document.body.scrollTop
+            };
+    
+            var top_offset = rsl.top - wpoffset;
+    
+            var actualY = e.pageY - wpoffset;
+    
+            // editor height
+            var eheight = actualY - top_offset;
+            
+            // Set wrapper height
+            document.getElementById(id_wrapper).style.height = eheight;
+            
+            // Set dragbar opacity while dragging (set to 0 to not show)
+            document.getElementById(id_dragbar).style.opacity =  0.15;
+        };
+    
+        document.getElementById(id_dragbar).addEventListener("mousedown", action_mousedown);
+     
+        var action_mouseup = function(e){
+            if (window.draggingAceEditor[id_editor])
+            {
+                var ctx_editor = document.getElementById(id_editor);
+                
+                var rect = ctx_editor.getBoundingClientRect();
+    
+                var rsl = {
+                    top: rect.top + document.body.scrollTop
+                };
+    
+                var actualY = e.pageY - wpoffset;
+                var top_offset = rsl.top - wpoffset;
+                var eheight = actualY - top_offset;
+                
+                document.removeEventListener("mousemove", action_document_mousemove);
+                
+                // Set dragbar opacity back to 1
+                document.getElementById(id_dragbar).style.opacity = 1;
+                
+                // Set height on actual editor element, and opacity back to 1
+                ctx_editor.style.height = eheight + "px";
+                ctx_editor.style.opacity = 1;
+                
+                // Trigger ace editor resize()
+                editor.resize();
+    
+                window.draggingAceEditor[id_editor] = false;
+            }
+        };
+    
+        document.addEventListener("mouseup", action_mouseup);
+    }
+        
+        
+    makeAceEditorResizable(aceEditorInstance);
+
     });
