75f20fddc4a5f21067cc4b82ed13d887171d1522
talehhasan <47014159+talehhasan@users.noreply.github.com>
2021-11-02 14:22:52 +0400

    Copy project

--- public_html/resource/js/bs4/a_1.js
+++ public_html/resource/js/bs4/a_1.js
@@ -17382,8 +17382,9 @@ function getBacklogListByProject4Element(projectId, elm) {
                     $(elm).append($('<option>').val(id).text(name))
                 }
 
-                $(elm).selectpicker("refresh")
+                $(elm).selectpicker("refresh");
             } catch (err) {
+                $(elm).selectpicker("refresh");
             }
         },
         error: function () {
