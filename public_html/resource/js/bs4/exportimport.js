db0f5df3396dbca80dbc5824bb278a275d1b8bc3
anar1984 <me@anarrustamov.com>
2022-07-16 18:01:09 +0400

    dddd

--- public_html/resource/js/bs4/exportimport.js
+++ public_html/resource/js/bs4/exportimport.js
@@ -16,7 +16,88 @@ const ExportImportTool = (function () {
             deploy_release_action(this);
         })
 
+        $(document).on('change', 'select.ei-project-gui-list', function () {
+            load_story_card_by_project(this);
+        })
 
+        $(document).on('change', 'select.ei-project-api-list', function () {
+            load_api_by_project(this);
+        })
+
+        $(document).on('click', '.ei-primary-gui-list', function () {
+            deploy_by_storycards(this);
+        })
+
+        $(document).on('click', '.ei-primary-api-list', function () {
+            deploy_by_api(this);
+        })
+
+    }
+
+    const deploy_by_api = function (el) {
+        var fkApiId = $('select.ei-storycard-api-list').val();
+        fkApiId = fkApiId.toString();
+        fkApiId = fkApiId.replace(/,/g, "|");
+
+
+        if (!fkApiId) return;
+
+        var select = $('.ei-span-api-list');
+        select.text('in progress');
+
+        callApi('22071510323200509813', { fkApiId: fkApiId }, true, function (res) {
+            select.text('Folder Name:' + res.kv.folderName);
+        })
+    }
+
+    const deploy_by_storycards = function (el) {
+        var storycard = $('select.ei-storycard-gui-list').val();
+        storycard = storycard.toString();
+        storycard = storycard.replace(/,/g, "|");
+
+
+        if (!storycard) return;
+
+        var select = $('.ei-span-gui-list');
+        select.text('in progress');
+
+        callApi('22071504522800394004', { fkStoryCardId: storycard }, true, function (res) {
+            select.text('Folder Name:' + res.kv.folderName);
+        })
+    }
+
+    const load_api_by_project = function (el) {
+        var fkProjectId = $(el).val();
+        if (!fkProjectId) return;
+
+        var select = $('select.ei-storycard-api-list');
+        select.html('')
+
+        callApi('22071616271502924994', { fkProjectId: fkProjectId }, true, function (res) {
+            res.tbl[0].r.map(function (o) {
+                var ln = `<option value='${o.id}'>${o.backlogName}</option>`;
+                select.append(ln);
+            })
+            select.attr('multiple', "true")
+            select.selectpicker('refresh');
+        })
+    }
+
+    const load_story_card_by_project = function (el) {
+        var fkProjectId = $(el).val();
+        if (!fkProjectId) return;
+
+        var select = $('select.ei-storycard-gui-list');
+        select.html('')
+
+        callApi('22030400352507334738', { fkProjectId: fkProjectId }, true, function (res) {
+            res.tbl[0].r.map(function (o) {
+                var ln = `<option value='${o.id}'>${o.backlogName}</option>`;
+                select.append(ln);
+            })
+            select.attr('multiple', "true")
+            select.selectpicker('refresh');
+        })
     }
 
     const deploy_release_action = function () {
@@ -62,18 +143,26 @@ const ExportImportTool = (function () {
     const export_tables = function (el) {
         var table_id = $('select.ei-table-list').val();
 
-        if (!table_id){ Toaster.showError("Tables are not selected.")}
-        
+        if (!table_id) { Toaster.showError("Tables are not selected.") }
+
         table_id = table_id.toString().replace(/,/g, '|');
         var data = {};
         data.fkTableId = table_id;
         data.tableStructure = $('.ei-only-table-sctructure').is(":checked") ? "1" : "";
         data.tableHistory = $('.ei-only-table-history').is(":checked") ? "1" : "";
         data.tableData = $('.ei-only-table-data').is(":checked") ? "1" : "";
+        data.fromDate = toDate('ei-from-date-list');
+        data.toDate = toDate('ei-to-date-list');
+
 
         $('.hoxe').html('In Progress');
         callApi('22051711163103472938', data, true, function (res) {
             var urlCommon = urlGl + "api/get/deploy/" + res.kv.db_structure;
+
+            $.get(urlCommon, function (data) {
+                $(".ei-sql-query").text(data);
+
+            });
 
             if (res.kv.db_structure)
                 $('.hoxe').html(`<a href='${urlCommon}' target='_blank'>${res.kv.db_structure}</a><br>`);
@@ -113,6 +202,30 @@ const ExportImportTool = (function () {
     const load = function () {
         load_db_list();
         load_deploy_release_list();
+        load_project_list();
+    }
+
+    const load_project_list = function () {
+        var select = $("select.ei-project-gui-list");
+        var select1 = $("select.ei-project-api-list");
+
+        select.html('<option></option>');
+        select1.html('<option></option>');
+
+
+        callApi("22071615512005378995", {}, true, function (res) {
+            res.tbl[0].r.map(function (o) {
+                var ln = `<option value='${o.id}'>${o.projectName}</option>`;
+
+                select.append(ln);
+                select1.append(ln);
+
+            })
+
+            select.selectpicker('refresh');
+            select1.selectpicker('refresh');
+
+        })
     }
 
     const load_deploy_release_list = function () {
